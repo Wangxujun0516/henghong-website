8902fd15fefd550ecdd34efea894efae67d749ef
Simon <wangxujun0516@gmail.com>
2026-06-12 09:17:46 +0800

    Add Umami Analytics tracking script

--- components/analytics/UmamiAnalytics.tsx
+++ components/analytics/UmamiAnalytics.tsx
@@ -3,11 +3,9 @@
 import Script from "next/script";
 
 const UMAMI_SCRIPT_URL = "https://cloud.umami.is/script.js";
-const UMAMI_WEBSITE_ID = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID;
+const UMAMI_WEBSITE_ID = "f57c2081-9e72-4c14-9895-c13ed5d8a381";
 
 export function UmamiAnalytics() {
-  if (!UMAMI_WEBSITE_ID) return null;
-
   return (
     <Script
       defer
